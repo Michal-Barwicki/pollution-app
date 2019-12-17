e589095b407578f971c85f79bf241b18c5384796
Michał Barwicki <m.barwicki93@gmail.com>
2019-12-17 08:05:24 +0100

    Updates

---
+++ precache-manifest.a9185254194f75938437511fab54af2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6780d9b7a8b7de363d45ae8b3d1fed0",
+    "url": "/pollution-app/index.html"
+  },
+  {
+    "revision": "7f2acde817cef1325329",
+    "url": "/pollution-app/static/css/main.090e5f5c.chunk.css"
+  },
+  {
+    "revision": "637a945bf040b913444a",
+    "url": "/pollution-app/static/js/2.84bcb3f6.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pollution-app/static/js/2.84bcb3f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f2acde817cef1325329",
+    "url": "/pollution-app/static/js/main.651f9d9b.chunk.js"
+  },
+  {
+    "revision": "c560350ade1869ba9de9",
+    "url": "/pollution-app/static/js/runtime-main.aabf3ed1.js"
+  },
+  {
+    "revision": "be0354d8180d8f54a405110756f7453b",
+    "url": "/pollution-app/static/media/industry.be0354d8.jpg"
+  }
+]);
